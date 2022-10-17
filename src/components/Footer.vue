8aa388f6a124e38297bb2c85858cfdda579838ca
CXTNUCE98 <79709491+CXTNUCE98@users.noreply.github.com>
2022-10-18 00:09:48 +0700

    first commit

---
+++ src/components/Footer.vue
@@ -0,0 +1,46 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<script setup lang="ts">
+// import {Search, ShoppingCart, UserFilled } from '@element-plus/icons-vue'
+// import {
+//   ElIcon
+// } from 'element-plus'
+</script>
+
+<template>
+  <div class="flex justify-between gap-8 items-center bg-[#d9d9d6]">
+    <div class="flex flex-1 justify-center">      
+      <div>
+        <h1 class="">
+          LIÊN HỆ
+        </h1>
+        <div>Mua hàng online: 0239 923 293</div>
+        <div>Liên hệ hợp tác: Chronosstore@gmail.com</div>
+        <div>Chăm sóc khách hàng: 0239 901 192</div>
+        <div>Thời gian làm việc: 8h30 - 21h00</div>
+      </div>
+    </div>
+    <div class="flex flex-1 justify-center">
+      <div>
+        <h1 class="">
+          THÀNH VIÊN
+        </h1>
+        <div>Quyền lợi thành viên</div>
+        <div>Thông tin thành viên</div>
+        <div>Theo dõi đơn hàng</div>
+        <div>Hướng dẫn mua hàng</div>
+      </div>
+    </div>
+    <div class="flex flex-1 justify-center">
+      <div>
+        <h1 class="">
+          KẾT NỐI
+        </h1>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
---
+++ src/components/Header.vue
@@ -0,0 +1,37 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<script setup lang="ts">
+import logo from '../assets/image/logo.jpg'
+import {Search, ShoppingCart, UserFilled } from '@element-plus/icons-vue'
+import {
+  ElIcon
+} from 'element-plus'
+</script>
+
+<template>
+  <div class="flex h-20 justify-between items-center ">
+    <div class="flex flex-1 justify-center gap-8">
+      <span>NEW ARRIVAL</span>
+      <span>COLLECTION</span>
+      <span>ABOUT US</span>
+    </div>
+    <div class="flex-1 flex justify-center">
+      <img
+        class="w-[164px] cursor-pointer "
+        :src="logo"
+      >
+    </div>
+    <div class="flex flex-1 justify-center gap-8">
+      <el-icon class="cursor-pointer">
+        <Search />
+      </el-icon>
+      <el-icon class="cursor-pointer">
+        <ShoppingCart />
+      </el-icon>
+      <el-icon class="cursor-pointer">
+        <UserFilled />
+      </el-icon>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
